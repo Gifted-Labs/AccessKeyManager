0143475993d2da566cffc14ea5919913345d1480
Gifted-Labs <juliusadjeteysowah@gmail.com>
2024-05-07 02:10:41 +0000

    This is the initial commit

---
+++ src/main/java/com/juls/accesskeymanager/AccesskeymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.juls.accesskeymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesskeymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesskeymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juls/accesskeymanager/AccesskeymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juls.accesskeymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesskeymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
